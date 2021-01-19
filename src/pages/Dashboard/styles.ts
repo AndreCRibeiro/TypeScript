0ea3f6fd8c94f175e57089458b2166ab6874a9ad
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2021-01-19 18:11:05 -0300

    feat dashboard page completed

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   line-height: 56px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -20,9 +24,15 @@ export const Form = styled.form`
     flex: 1;
     height: 70px;
     padding: 0 24px;
+    margin-right: 3px;
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3A3A3A;
+    border: 2px solid #fff;
+
+    ${(props) => props.hasError && css`
+      border-color: #c53030;
+    `}
 
     &::placeholder {
       color: #A8A8B3;
@@ -96,4 +106,10 @@ export const Repositories = styled.div`
       color: #CBCBD6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #C53030;
+  margin: 8px 0px 0px 20px;
 `;

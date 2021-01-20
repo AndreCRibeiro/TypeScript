a573c352f633ffab1228f7390d4a07ee7a8d2f0e
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2021-01-19 23:37:16 -0300

    feat finished

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
@@ -18,7 +19,18 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return []
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+  }, [repositories])
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -59,7 +71,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repo => (
-          <a key={repo.full_name} href="teste">
+          <Link key={repo.full_name} to={`/repository/${repo.full_name}`}>
             <img
               src={repo.owner.avatar_url}
               alt={repo.owner.login}
@@ -70,7 +82,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
 
       </Repositories>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  name: string;
+}
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issues {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issues[]>([])
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.name}`).then(response => {
+      setRepository(response.data);
+    })
+
+    api.get(`repos/${params.name}/issues`).then(response => {
+      setIssues(response.data);
+    })
+
+    // Título de curiosidade
+    // Promise.race é bem interessante
+    /*async function loadData(): Promise<void> {
+      const [repository, issues] = await Promise.all([
+        api.get(`repos/${params.name}`),
+        api.get(`repos/${params.name}/issues`)
+      ])
+    }*/
+  }, [params.name])
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      {
+        repository && (
+          <RepositoryInfo>
+            <header>
+              <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+            </header>
+            <ul>
+              <li>
+                <strong>{repository.stargazers_count}</strong>
+                <span>Stars</span>
+              </li>
+              <li>
+                <strong>{repository.forks_count}</strong>
+                <span>Forks</span>
+              </li>
+              <li>
+                <strong>{repository.open_issues_count}</strong>
+                <span>Issues abertas</span>
+              </li>
+            </ul>
+          </RepositoryInfo>
+        )
+      }
+      <Issues>
+        {issues.map(issue => (
+          <Link key={issue.id} to={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Issues>
+    </>
+  )
 }
 
 export default Repository;
